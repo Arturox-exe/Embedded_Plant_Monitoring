584bb3d6fce38bf25d2aeb51219b4b2a63ddf8d5
cxrLubi97 <cesar.rodriguezser@alumnos.upm.es>
2021-11-04 10:34:12 +0100

    nuevos cambios
    venga que pusheo

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
+
+/*******************************************************************************
+ *******************************  Includes  ***********************************
+ ******************************************************************************/
 #include "mbed.h"
+#include "MMA8451Q.h"
 
 
 
@@ -28,8 +33,11 @@ extern void ReadADC(void);
 
 
 int main() {
+	float result[3] = {0,0,0};
+	MMA8451Q acc(PB_9,PB_8,0x1c<<1);
+	int present = acc.getWhoAmI();
   while (1){
-				wait_us(1000000);
+			wait_us(1000000);
 			if(RTHpresent()){
 					printf("Temperature: %d ,%3d oC\r\n", _tData / 1000, _tData % 1000);
 					printf("Humidity: %d,%3d%\r\n", _rhData / 1000, _rhData % 1000);
@@ -41,6 +49,13 @@ int main() {
 			 ReadADC();
 			 printf("Light: %.2f % \n", light_value);
 			 printf("Moisture:  %.2f % \n", moisture_value);
-
-}
+			
+			if(present == 0x1A){
+				acc.getAccAllAxis(result);
+				printf("Rotation: x =%f \t y=%f\t z=%f\n",result[0],result[1],result[2]);
+			}else{
+				printf("Please connect accelerometer");
+				present = acc.getWhoAmI();
+			}
 	}
+}
