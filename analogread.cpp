6d0b146b5f1c72a2651f5b8929b6e4367923b314
Arturox-exe <arturo.herrera.carmena@gmail.com>
2021-11-13 19:02:14 +0100

    Limpieza de codigo

--- analogread.cpp
+++ analogread.cpp
@@ -3,10 +3,6 @@
 #include <cstdio>
 #include <string>
 
-#define RAISED  0;
-#define LOWERING 1;
-#define DOWN 2;
-#define RISING 3;
 
 AnalogIn light(PA_4);
 AnalogIn moisture(PA_0);
@@ -17,23 +13,7 @@ float moisture_value;
 
 
 bool checker = false;
-bool printer_checker;
-bool up = true;
-bool summer = true;
-int state = RAISED;
-string s_state = "RAISED";
 
-Ticker sample;
-Ticker printer;
-Timeout timer;
-
-void tick_isr(void) {checker = true;}
-void printer_isr(void) {printer_checker = true;}
-void timer_isr(void){up = !up;}
-void summer_winter(void){summer = !summer;}
-
-Thread thread;
-Thread thread_2;
 
 void ReadADC(void) {
             light_value = light.read_u16()*100.00/65536.00;
--- main.cpp
+++ main.cpp
@@ -450,36 +450,36 @@ int main(void) {
 						printf("COLOR SENSOR: Clear: %d, Red: %d, Green: %d, Blue: %d\n",rgb_readings[0],rgb_readings[1],rgb_readings[2],rgb_readings[3]);
 								if(rgb_readings[0]>6000)
 							{
-									Red    = 0;
-									Green  = 0;
-									Blue   = 0;		
+									Red    = 1;
+									Green  = 1;
+									Blue   = 1;		
 							}
 							else
 								{
 									if(rgb_readings[1]<100 && rgb_readings[2]<100 && rgb_readings[3]<100) //If low: none
 										{
-											Red    = 1;
-											Green  = 1;
-											Blue   = 1;
+											Red    = 0;
+											Green  = 0;
+											Blue   = 0;
 										}
 										else
 											{
 											if(rgb_readings[1]>rgb_readings[2] && rgb_readings[1]>=rgb_readings[3]) //If max=RED
 												{
-													Red    = 0;
-													Green  = 1;
-													Blue   = 1;
-												}else if(rgb_readings[2]>rgb_readings[1] && rgb_readings[2]>rgb_readings[3]) //If max=Green
-												{
 													Red    = 1;
 													Green  = 0;
-													Blue   = 1;
+													Blue   = 0;
+												}else if(rgb_readings[2]>rgb_readings[1] && rgb_readings[2]>rgb_readings[3]) //If max=Green
+												{
+													Red    = 0;
+													Green  = 1;
+													Blue   = 0;
 												}
 												else if(rgb_readings[3]>rgb_readings[1] && rgb_readings[3]>rgb_readings[2])   //If max=Blue
 												{
-													Red    = 1;
-													Green  = 1;
-													Blue   = 0;						
+													Red    = 0;
+													Green  = 0;
+													Blue   = 1;						
 												}
 											}
 								}
